445616fb9d3d6dcc695fa4366c58465d1c6de7b5
pranathi000 <singarajupranathi@gmail.com>
2021-09-11 14:30:58 +0530

    one line added

---
+++ tutotials/tut59.py
@@ -0,0 +1,10 @@
+# abstraction and encapsulation.
+print("A brief about the object oriented programming")
+# ENCAPSULATION cheezon ko tukdom mein thodd lenaa Encapsulation hotha hai.
+# Layer of abstractions and many more. these two are mostly used in making games.
+# A group of programmers use this Encapsulation for making games and  vagaira.
+# kisi bhi cheez ki Implementation details ko hide karna encapsulation ki madadh se hotha hai.
+# ABSTRACTION : hiding the unimportant data and revealing only the important ones.
+# ENCAPSULATION : hiding the data for the purpose of Protection.
+# Real life examples of encapsulation. *Encryption
+#                       abstraction.   *Tv, laptop
