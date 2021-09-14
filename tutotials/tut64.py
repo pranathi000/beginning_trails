be85dd6ef0654585c7d24dd853e362ec35bbc863
pranathi000 <singarajupranathi@gmail.com>
2021-09-14 16:38:52 +0530

    one line added!

---
+++ tutotials/tut64.py
@@ -0,0 +1,10 @@
+print("THE TOPIC IS ABOUT POLYMORPHISM")
+
+# what is polymorphism?
+# it is neither a function, nor a class nor a built-in-module.
+# it is just simply one of the concept that we should learn in python.
+# it means that ability to take various forms.
+
+print(5+6)  #int obj
+print("5" + "6") #srt obj
+# until we have read all about abstraction, encapsulation, inheritance and also about polymorphism.
