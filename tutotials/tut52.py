79640bfb4fd767933368edfdc387b53cc8527ce4
pranathi000 <singarajupranathi@gmail.com>
2021-09-21 13:59:31 +0530

    one line added!

---
+++ tutotials/tut52.py
@@ -0,0 +1,32 @@
+# this is all about object oriented programming.
+# o o p is  a way to make objects in the ccode.
+# objects is nothing but it is derived from the name class tab.
+# classes - template
+# onject - letter - instance of the class
+# DRY - do not repeat yourself
+#  time money and effort got save if we choose this type of programming in our coding life.
+# TIME MONEY AND EFFORT
+
+# go and start OBJECT ORIENTED PROGRAMMIN IN THE NEXT VEDIO CLASSES
+
+# ENCAPSULATION: wrapping or binding up of variables(data_ and methods(code) into a single unit which is useful for security.
+
+# CLASS: That boundary name is class. all the encapsulation we are doing will be done inside the class.
+# class provies the definition of the object.
+# collection od objects.
+# it definnes bject.
+
+# OBJECT: class and onject are interrrelated with each other.
+# it is also called a sample.
+# class is imaginary and object is real thing.
+# collection of objects is nothing but a class.
+
+# DATA ABSTRACTION: representiong essential features without including background details.
+
+# INHERITANCE: deriving a new class from already exxisting class.
+
+# POLYMORPHISM:  many forms.
+# an operation exibits different behaviours in different instances.
+
+
+
