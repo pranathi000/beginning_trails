d4bae7c3eabf36102919587fefb31fb7b114dcd9
pranathi000 <singarajupranathi@gmail.com>
2021-09-21 13:56:32 +0530

    one line added!

---
+++ tutotials/tut53.py
@@ -0,0 +1,20 @@
+print(" THIS VEDIO IS ABOUT OOPS LECTURE 1")
+
+
+class student:
+    pass
+
+harry = student()
+larry = student()
+
+
+harry.name = "harry"
+harry.std = 12
+harry.section = 1
+larry.std = 11
+larry.subjects = ["hindi", "english"]
+print(harry, larry)
+print(harry.name)
+print(harry.std)
+print(harry.section)
+print(harry.std ,larry.std , larry.subjects)
