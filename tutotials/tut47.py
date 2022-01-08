8a38c6f2e0891c3927c4012edd61d300263652df
pranathi000 <singarajupranathi@gmail.com>
2022-01-08 20:09:16 +0530

    one line added!

---
+++ tutotials/tut47.py
@@ -0,0 +1,10 @@
+# join function in python.
+# code with harry....
+list= ["jhon", "cena","randy", "orton", "sheamus", "khali", "jindar mahal"]
+# for item in list:
+#     print(item, "and", end=" ")
+a = " , ".join(list)
+
+
+print(a, "and other super stars.")
+# end is used for getting all of them in a same line.
