d8c475ff9ed7eac79db7f9ab058136c40f92b536
pranathi000 <singarajupranathi@gmail.com>
2022-01-08 20:11:23 +0530

    one line added!

---
+++ tutotials/tut46-1.py
@@ -0,0 +1,12 @@
+def printhar(string):
+    return "ye string harry ko dede thakur ()"
+
+def add(num1, num2):
+    return num1 + num2 +5
+
+
+print("and the name is", __name__)
+if __name__ == '__main__':
+    print(printhar("harry1"))
+    o = add(4, 6)
+    print(o)
