3684414cdf68453c8cba7dab9b62499b6b56b950
pranathi000 <singarajupranathi@gmail.com>
2022-01-08 20:03:35 +0530

    one line added!

---
+++ tutotials/tut72.py
@@ -0,0 +1,26 @@
+print("THE TOPIC IS ABOUT GENERATORS")
+"""
+Iterable - __iter__() or __getitem__()
+Iterator -  __next__()
+Iteration - 
+
+"""
+
+# generator is a type of iterator.
+def gen():
+    for i in range(78):
+        yield i
+g = gen(345345345345345345345343)
+print(g)
+
+
+
+
+
+
+
+
+
+
+
+
